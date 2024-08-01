4b46bcfd87ebd073197cd26ad296efe44c6434bd
Thuyen Code <thuyencode@proton.me>
2024-08-01 13:57:03 +0700

    Make `header` a reuseable template

--- src/modules/home/home.routes.ts
+++ src/modules/home/home.routes.ts
@@ -3,7 +3,13 @@ import e from 'express'
 const home_routes = e.Router()
 
 home_routes.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    navigation: {
+      href: '/new',
+      name: 'New Message',
+      icon_name: 'mdi:message-add'
+    }
+  })
 })
 
 export default home_routes
--- src/modules/new/new.routes.ts
+++ src/modules/new/new.routes.ts
@@ -3,7 +3,13 @@ import e from 'express'
 const new_routes = e.Router()
 
 new_routes.get('/new', (req, res) => {
-  res.render('new')
+  res.render('new', {
+    navigation: {
+      href: '/',
+      name: 'Home',
+      icon_name: 'mdi:home'
+    }
+  })
 })
 
 export default new_routes
